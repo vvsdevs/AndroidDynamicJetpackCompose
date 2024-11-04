7370b1f4721c9c4f5ccaeb0ce0f0a45330d4faf2
Venkatadri Singanamala <venkatadri.s@ext.dazn.com>
2024-11-04 18:37:28 +0530

    Import issues with new api designs added...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.demo.dynamiccompose"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
